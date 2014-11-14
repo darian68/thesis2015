67f67116b366c9b41142fc2ee4b2d22079a51e22
dai.ung <dai.n.ung@gmail.com>
2014-11-14 23:45:10 +0700

    Initialize thesis project

---
+++ 07-Source_Code/HAR/HAR/Program.cs
@@ -0,0 +1,13 @@
+﻿using SlimDX.Windows;
+
+namespace HAR
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var form = new RenderForm("Tutorial 1: Basic Window");
+            MessagePump.Run(form, () => { });
+        }
+    }
+}
